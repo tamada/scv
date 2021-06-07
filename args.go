c478df25977e5848f04ab1a5cd7b764de05e5e02
Haruaki Tamada <tamada@users.noreply.github.com>
2021-06-07 22:36:36 +0900

    implement parsing cli options

---
+++ args.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	flag "github.com/spf13/pflag"
+)
+
+type options struct {
+	algorithm string
+	format    string
+	inputType string
+	helpFlag  bool
+	args      []string
+}
+
+func isIn(originalValue string, set []string) error {
+	value := strings.ToLower(originalValue)
+	for _, element := range set {
+		if element == value {
+			return nil
+		}
+	}
+	return fmt.Errorf("%s: unavailable value, availables: %v", originalValue, set)
+}
+
+func validateAlgorithm(algorithm string) error {
+	return isIn(algorithm, []string{"simpson", "jaccard", "dice", "cosine"})
+}
+
+func validateInputType(inputType string) error {
+	return isIn(inputType, []string{"default", "string", "file", "json"})
+}
+
+func validateFormat(format string) error {
+	return isIn(format, []string{"default", "json", "xml"})
+}
+
+func validateEachOpt(opts *options) (*options, error) {
+	data := []struct {
+		value     string
+		validator func(value string) error
+	}{
+		{opts.algorithm, validateAlgorithm},
+		{opts.inputType, validateInputType},
+		{opts.format, validateFormat},
+	}
+	for _, datum := range data {
+		if err := datum.validator(datum.value); err != nil {
+			return nil, err
+		}
+	}
+	return opts, nil
+}
+
+func validate(opts *options) (*options, error) {
+	if opts.helpFlag {
+		return opts, nil
+	}
+	if len(opts.args) <= 1 {
+		return nil, fmt.Errorf("two arguments are required at the least")
+	}
+	return validateEachOpt(opts)
+}
+
+func parseArgs(args []string) (*options, error) {
+	opts := &options{}
+	flags := flag.NewFlagSet("scv", flag.ContinueOnError)
+	flags.Usage = func() { fmt.Println(helpMessage(args[0])) }
+	flags.StringVarP(&opts.algorithm, "algorithm", "a", "default", "specifies the calculating algorithm.")
+	flags.StringVarP(&opts.format, "format", "f", "default", "specifies the output format.")
+	flags.StringVarP(&opts.inputType, "input-type", "t", "string", "specifies the type of VECTORS.")
+	flags.BoolVarP(&opts.helpFlag, "help", "h", false, "prints this message")
+	if err := flags.Parse(args); err != nil {
+		return nil, err
+	}
+	opts.args = flags.Args()
+	return validate(opts)
+}
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/tamada/scv/vector"
 )
 
 func helpMessage(originalProgramName string) string {
@@ -15,7 +17,7 @@ OPTIONS
                                    Available values are: simpson, jaccard, dice, and cosine.
     -f, --format <FORMAT>          specifies the resultant format. Default is default.
                                    Available values are: default, json, and xml.
-    -t, --input-type <TYPE>        specifies the type of VECTORS. Default is file.
+    -t, --input-type <TYPE>        specifies the type of VECTORS. Default is string.
                                    If TYPE is separated with comma, each type shows
                                    the corresponding VECTORS.
                                    Available values are: file, string, and json.
@@ -24,9 +26,73 @@ VECTORS
     the source of vectors for calculation.`, name)
 }
 
-func goMain(args []string) int {
-	fmt.Println(helpMessage(args[0]))
+func convert(opts *options) []*vector.Vector {
+	results := []*vector.Vector{}
+	for _, arg := range opts.args {
+		vector := vector.NewVectorFromString(arg)
+		results = append(results, vector)
+	}
+	return results
+}
+
+func pairing(vectors []*vector.Vector) []*vector.VectorPair {
+	pairs := []*vector.VectorPair{}
+	for _, vector1 := range vectors {
+		for _, vector2 := range vectors {
+			if vector1 == vector2 {
+				break
+			}
+			pairs = append(pairs, &vector.VectorPair{Vector1: vector1, Vector2: vector2})
+		}
+	}
+	return pairs
+}
+
+type result struct {
+	similarity float32
+	err        error
+}
+
+func calculate(pairs []*vector.VectorPair, algorithm vector.Algorithm) []*result {
+	results := []*result{}
+	for _, pair := range pairs {
+		similarity := pair.Compare(algorithm)
+		results = append(results, &result{similarity: similarity, err: nil})
+	}
+	return results
+}
+
+func printResult(algorithmName string, pair *vector.VectorPair, r *result) {
+	fmt.Printf("%s(%s, %s): %f\n", algorithmName, pair.Vector1.Source.Value(), pair.Vector2.Source.Value(), r.similarity)
+}
+
+func perform(opts *options) int {
+	vectors := convert(opts)
+	pairs := pairing(vectors)
+	algorithm, err := vector.NewAlgorithm(opts.algorithm)
+	if err != nil {
+		fmt.Println(err.Error())
+		return 3
+	}
+	results := calculate(pairs, algorithm)
+	for i, _ := range results {
+		printResult(opts.algorithm, pairs[i], results[i])
+	}
 	return 0
+}
+
+func goMain(args []string) int {
+	opts, err := parseArgs(args)
+	if err != nil {
+		fmt.Printf("%s: %s\n", filepath.Base(args[0]), err.Error())
+		fmt.Println(helpMessage(args[0]))
+		return 1
+	}
+	if opts.helpFlag {
+		fmt.Println(helpMessage(args[0]))
+		return 0
+	}
+	return perform(opts)
 }
 
 func main() {
